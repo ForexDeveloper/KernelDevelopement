a3eaf6901579dabe1f88aa6de83cba50e2dd765d
JavidPour.Mohammad <JavidPour.Mohammad@TEK001N4446>
2023-11-20 20:51:11 +0330

    push

---
+++ Foodzilla.Kernel/Patch/PatchDbContext.cs
@@ -0,0 +1,8 @@
+﻿using Foodzilla.Kernel.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Foodzilla.Kernel.Patch;
+
+public sealed class PatchDbContext<TContext, TEntity> where TContext : DbContext where TEntity : Entity, IPatchValidator
+{
+}
