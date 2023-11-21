89aeea685feb28c54d795cc32b4a2b8ec7f7a383
JavidPour.Mohammad <JavidPour.Mohammad@TEK001N4446>
2023-11-21 14:42:57 +0330

    Add TeamLeads

--- Foodzilla.Kernel.UnitTest/Domain/ChiefOfficers/ChiefProductOfficer.cs
+++ Foodzilla.Kernel.UnitTest/Domain/ChiefOfficers/ChiefProductOfficer.cs
@@ -1,4 +1,5 @@
 ﻿using Foodzilla.Kernel.Patch;
+using Foodzilla.Kernel.UnitTest.Domain.TeamLeads;
 
 namespace Foodzilla.Kernel.UnitTest.Domain.ChiefOfficers;
 
@@ -7,6 +8,8 @@ public sealed class ChiefProductOfficer : ChiefUnitIdentity, IPatchValidator
     public int ChiefExecutiveOfficerId { get; private set; }
 
     public ChiefExecutiveOfficer? ChiefExecutiveOfficer { get; private set; }
+
+    public IReadOnlyCollection<ScrumMasterTeamLead>? ScrumMasterTeamLeads { get; private set; }
 
     private ChiefProductOfficer(string name, string lastName, string nationalCode, string personalCode, string address, int age, int? daysOfVacation, decimal? height, decimal? weight, bool isFired, Guid uniqueIdentifier, EyeColor? eyeColor, Graduation graduation, Experience experience, DateTimeOffset? modifiedDate, DateTimeOffset birthDate, DateTimeOffset contraDateEnd, DateTimeOffset contraDateStart, string assignedOrganization)
         : base(name, lastName, nationalCode, personalCode, address, age, daysOfVacation, height, weight, isFired, uniqueIdentifier, eyeColor, graduation, experience, modifiedDate, birthDate, contraDateEnd, contraDateStart, assignedOrganization)
--- Foodzilla.Kernel.UnitTest/Domain/ChiefOfficers/ChiefTechnicalOfficer.cs
+++ Foodzilla.Kernel.UnitTest/Domain/ChiefOfficers/ChiefTechnicalOfficer.cs
@@ -1,4 +1,5 @@
 ﻿using Foodzilla.Kernel.Patch;
+using Foodzilla.Kernel.UnitTest.Domain.TeamLeads;
 
 namespace Foodzilla.Kernel.UnitTest.Domain.ChiefOfficers;
 
@@ -7,6 +8,8 @@ public sealed class ChiefTechnicalOfficer : ChiefUnitIdentity, IPatchValidator
     public int ChiefExecutiveOfficerId { get; private set; }
 
     public ChiefExecutiveOfficer? ChiefExecutiveOfficer { get; private set; }
+
+    public IReadOnlyCollection<TechnicalTeamLead>? TechnicalTeamLeads { get; private set; }
 
     private ChiefTechnicalOfficer(string name, string lastName, string nationalCode, string personalCode, string address, int age, int? daysOfVacation, decimal? height, decimal? weight, bool isFired, Guid uniqueIdentifier, EyeColor? eyeColor, Graduation graduation, Experience experience, DateTimeOffset? modifiedDate, DateTimeOffset birthDate, DateTimeOffset contraDateEnd, DateTimeOffset contraDateStart, string assignedOrganization)
         : base(name, lastName, nationalCode, personalCode, address, age, daysOfVacation, height, weight, isFired, uniqueIdentifier, eyeColor, graduation, experience, modifiedDate, birthDate, contraDateEnd, contraDateStart, assignedOrganization)
