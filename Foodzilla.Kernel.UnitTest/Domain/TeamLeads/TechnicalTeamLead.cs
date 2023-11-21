13e92863278cfcc9da6d49872768e38a919e9b36
JavidPour.Mohammad <JavidPour.Mohammad@TEK001N4446>
2023-11-21 14:54:40 +0330

    .

--- Foodzilla.Kernel.UnitTest/Domain/TeamLeads/TechnicalTeamLead.cs
+++ Foodzilla.Kernel.UnitTest/Domain/TeamLeads/TechnicalTeamLead.cs
@@ -1,9 +1,12 @@
 ﻿using Foodzilla.Kernel.Patch;
+using Foodzilla.Kernel.UnitTest.Domain.Seniors;
 
 namespace Foodzilla.Kernel.UnitTest.Domain.TeamLeads;
 
 public sealed class TechnicalTeamLead : Identity<int>, IPatchValidator
 {
+    public IReadOnlyCollection<SeniorDeveloper>? Seniors { get; private set; }
+
     private TechnicalTeamLead(string name, string lastName, string nationalCode, string personalCode, string address, int age, int? daysOfVacation, decimal? height, decimal? weight, bool isFired, Guid uniqueIdentifier, EyeColor? eyeColor, Graduation graduation, Experience experience, DateTimeOffset? modifiedDate, DateTimeOffset birthDate, DateTimeOffset contraDateEnd, DateTimeOffset contraDateStart)
         : base(name, lastName, nationalCode, personalCode, address, age, daysOfVacation, height, weight, isFired, uniqueIdentifier, eyeColor, graduation, experience, modifiedDate, birthDate, contraDateEnd, contraDateStart)
     {
